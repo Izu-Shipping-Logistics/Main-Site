d0ce5ea1e262647fcee41896126cb9b4f2b26ae0
AltafDeveloper <123882585+altafchaudharykhan@users.noreply.github.com>
2024-01-24 22:54:18 +0530

    changes timeline and vision and mission

--- assets/js/main.js
+++ assets/js/main.js
@@ -170,7 +170,7 @@
             spaceBetween: 50,
             slidesPerGroup: 1,
             loop: true,
-            autoplay: false,
+            autoplay: true,
             grabCursor: true,
             speed: 400,
             breakpoints: {
@@ -198,7 +198,7 @@
             slidesPerGroup: 1,
             loop: true,
             autoplay:true,
-            grabCursor: true,
+            grabCursor:true,
             speed: 400,
             breakpoints: {
                 320: {
@@ -290,7 +290,7 @@
             spaceBetween: 25,
             slidesPerGroup: 1,
             loop: true,
-            autoplay: false,
+            autoplay: true,
             grabCursor: true,
             speed: 600,
             pagination: {
@@ -413,3 +413,76 @@
     });
 
 })(jQuery);
+
+(function ($) {
+    $(function () {
+  
+  
+      $(window).on('scroll', function () {
+        fnOnScroll();
+      });
+  
+      $(window).on('resize', function () {
+        fnOnResize();
+      });
+  
+  
+      var agTimeline = $('.js-timeline'),
+        agTimelineLine = $('.js-timeline_line'),
+        agTimelineLineProgress = $('.js-timeline_line-progress'),
+        agTimelinePoint = $('.js-timeline-card_point-box'),
+        agTimelineItem = $('.js-timeline_item'),
+        agOuterHeight = $(window).outerHeight(),
+        agHeight = $(window).height(),
+        f = -1,
+        agFlag = false;
+  
+      function fnOnScroll() {
+        agPosY = $(window).scrollTop();
+  
+        fnUpdateFrame();
+      }
+  
+      function fnOnResize() {
+        agPosY = $(window).scrollTop();
+        agHeight = $(window).height();
+  
+        fnUpdateFrame();
+      }
+  
+      function fnUpdateWindow() {
+        agFlag = false;
+  
+        agTimelineLine.css({
+          top: agTimelineItem.first().find(agTimelinePoint).offset().top - agTimelineItem.first().offset().top,
+          bottom: agTimeline.offset().top + agTimeline.outerHeight() - agTimelineItem.last().find(agTimelinePoint).offset().top
+        });
+  
+        f !== agPosY && (f = agPosY, agHeight, fnUpdateProgress());
+      }
+  
+      function fnUpdateProgress() {
+        var agTop = agTimelineItem.last().find(agTimelinePoint).offset().top;
+  
+        i = agTop + agPosY - $(window).scrollTop();
+        a = agTimelineLineProgress.offset().top + agPosY - $(window).scrollTop();
+        n = agPosY - a + agOuterHeight / 2;
+        i <= agPosY + agOuterHeight / 2 && (n = i - a);
+        agTimelineLineProgress.css({height: n + "px"});
+  
+        agTimelineItem.each(function () {
+          var agTop = $(this).find(agTimelinePoint).offset().top;
+  
+          (agTop + agPosY - $(window).scrollTop()) < agPosY + .5 * agOuterHeight ? $(this).addClass('js-ag-active') : $(this).removeClass('js-ag-active');
+        })
+      }
+  
+      function fnUpdateFrame() {
+        agFlag || requestAnimationFrame(fnUpdateWindow);
+        agFlag = true;
+      }
+  
+  
+    });
+  })(jQuery);
+  
